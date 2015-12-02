737e2208478eaeae2406ac27a79598cc670e0e00
Aliaksei Krasikau <a.krasikau@gmail.com>
2015-12-02 19:21:43 +0300

    Remove test methods

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,14 +4,6 @@ import {createUser} from '../controllers/userController';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-router.get('/secured', ensureAuthenticated, (req, res) => {
-  res.send('secured');
-});
-
 router.post('/users', createUser);
 
 router.post('/authenticate', authenticate);
