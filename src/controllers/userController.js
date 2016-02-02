1d9624ca9b0d7cd2a0c71ce78bd42aaf7c3542dc
Aliaksei Krasikau <a.krasikau@gmail.com>
2016-02-02 22:08:13 +0300

    Auth user after create

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
+import jwt from 'jsonwebtoken';
 import User from '../models/user';
+import config from '../config';
 
 export function createUser(req, res) {
   const { name, phoneNumber, password, email, cityId } = req.body;
@@ -10,7 +12,17 @@ export function createUser(req, res) {
       email,
       cityId
     })
-    .then(user => res.json({
-      user
-    }));
+    .then(user => {
+      let userInfo = {
+        id: user._id
+      };
+      let token = jwt.sign(userInfo, config.secret, {
+        expiresIn: 86400
+      });
+
+      res.json({
+        user,
+        token
+      });
+    });
 }
