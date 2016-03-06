3a6af9612c77f5856e0fb419be72c1b18ee06d60
Aliaksei Krasikau <a.krasikau@gmail.com>
2016-03-06 21:56:55 +0300

    Add role property to user schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,11 @@
-import mongoose, {Schema} from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
 let userSchema = new Schema({
   phoneNumber: String,
   name: String,
   lastName: String,
-  password: String
+  password: String,
+  role: String
 });
 
 export default mongoose.model('User', userSchema);
